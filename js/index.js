ea874538dc7a3096b0c59fb353872b7c256dbb1b
Clovis Sampaio Veiga Junior <94051504+csvjunior@users.noreply.github.com>
2022-09-10 16:27:14 -0300

    primeira versão com js site dindin

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const corHeader = document.querySelector("header");
+const linkCurso = document.querySelector("#menu_opcoes nav a");
+const introducao = document.querySelector("#introducao");
+
+
+corHeader.style.backgroundColor = "#2E948A";
+
+linkCurso.setAttribute("href", "cursos.html");
+
+introducao.style.justifyContent = "space-between";
